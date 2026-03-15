77639ead8d6f975f4260e193b0abda1bc6bbc808
Markus Golec <56672386+mgolec225@users.noreply.github.com>
2026-03-15 17:49:35 +0100

    Update name.c
    XDJ200SX

--- arduino/name.c
+++ arduino/name.c
@@ -1,6 +1,6 @@
 #include "usb_names.h"
 
-#define MIDI_NAME   {'X','D','J','1','0','0','S','X'}
+#define MIDI_NAME   {'X','D','J','2','0','0','S','X'}
 #define MIDI_NAME_LEN  8
 
 struct usb_string_descriptor_struct usb_string_product_name = {
